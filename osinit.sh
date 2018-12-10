47cebb073ea59306a59017029171a53785352ace
xinli16 <xinli16@gmail.com>
2018-12-10 17:22:40 +0800

    Create osinit.sh

---
+++ osinit.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#############################################################################
+# @file: osinit.sh - the script to setup cloud system
+# @author: xinli zhang
+# @version: 1.0
+# @create: 2018.12.01
+# @latest: 2018.12.01
+#############################################################################
+
+# 1. Install Ubuntu 18.04.01 LTS server with user: xl and password: zhang
+
+# 2. Create x-folder and write sn and name of system
+if [ 1 -gt 0 ];then
+xpath="/home/x"
+xfile=$xpath"/x"
+xflag="xc-ubu"
+sn="XC20080001"
+name="xc01"
+
+if [ -d $xpath ]; then
+rm -rf $xpath
+fi
+mkdir $xpath
+
+echo ${xflag} >> ${xfile}
+echo ${sn} >> ${xfile}
+echo ${name} >> ${xfile}
+fi
+
+# 3. Copy files
+if [ 0 -gt 0 ];then
+cp xc.sh $xpath
+fi
